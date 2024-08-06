17f76ed5ae0a511e156b86348d3572e1b67c6d6e
gabrielrih <usuario@MacBook-Air-de-Usuario.local>
2024-08-05 21:33:52 -0300

    fix: add missing variables

--- iac/prd/main.tf
+++ iac/prd/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 module "lambda" {
     source = "../modules/lambda"
-    aws_region = ""
-    lambda_zip_path = ""
+    aws_region = var.aws_region
+    lambda_zip_path = var.lambda_zip_path
 }
