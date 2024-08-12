9868566f9d8547d8d981d48f532c029874be05c7
gabrielrih <usuario@MacBook-Air-de-Usuario.local>
2024-08-12 20:25:21 -0300

    feat: deploy lambda on aws

--- iac/modules/lambda/lambda.tf
+++ iac/modules/lambda/lambda.tf
@@ -1,19 +1,19 @@
-# resource "aws_lambda_function" "language_detector" {
-#     function_name = "language-detector"
-#     filename = var.lambda_zip_path
-#     handler = "lambda.handler"  # main file + main function
-#     runtime = "python3.11"
-#     memory_size = 256
-#     source_code_hash = filebase64sha256("${var.lambda_zip_path}")
-#     ephemeral_storage {
-#       size = 512
-#     }
-#     role = aws_iam_role.language_detector.arn
-#     layers = [
-#         "arn:aws:lambda:${var.aws_region}:553035198032:layer:git-lambda2:8"
-#     ]
-#     timeout = 120
-# }
+resource "aws_lambda_function" "language_detector" {
+    function_name = "language-detector"
+    filename = var.lambda_zip_path
+    handler = "lambda.handler"  # main file + main function
+    runtime = "python3.11"
+    memory_size = 256
+    source_code_hash = filebase64sha256("${var.lambda_zip_path}")
+    ephemeral_storage {
+      size = 512
+    }
+    role = aws_iam_role.language_detector.arn
+    layers = [
+        "arn:aws:lambda:${var.aws_region}:553035198032:layer:git-lambda2:8"
+    ]
+    timeout = 120
+}
 
 resource "aws_iam_role" "language_detector" {
     name = "language-detector-role"
